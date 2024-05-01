4a22eaa06c81634906bde2b5eedadd4bc5adbfdb
chupapa327 <chupapa327@rambler.ru>
2024-05-01 12:51:28 +0500

    add

--- obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3bfd1a621aeb5f6b8f0d99314eae63c025df3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b1270c61c5db78c254b4e3f35e8a6ac93acc1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
